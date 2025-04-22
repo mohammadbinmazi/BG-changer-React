704d27492fb9130e1a08e61f59dbebf6d26da195
mohammad bin mazi <mohammadbinmazi8@gmail.com>
2025-04-22 12:23:53 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 
 function App() {
   const [color, setcolor] = useState("olive");
-  // function changeColor(color) {
-  //   setcolor(color);
-  // }
 
   return (
     <div
